8affbe7a4364d87df2714e5b3aed9719d6d88831
alejandro314 <manuelmartines385@gmail.com>
2025-07-29 20:04:37 -0500

    yes

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const express = require ('express')//importa el modulo de express
-const router = express.Router();//crea un enrutador de express 
-const controlador = require ('../controllers/usuarioController')//importa el controlador de usuario
-
-router.get("/usuario/todos", controlador.obtenerUsuarios);
-
-//ruta para logear usuarios
-router.post('/usuario/ingresar',controlador.ingresarUsuario);
-
-//ruta para registar usuarios
-router.post('/usuario/registrar/', controlador.registrarUsuario)
-
-module.exports=router;//export las rutas para que puedan sser utilizados en index.js
-=======
 
 const express = require('express');
 const router = express.Router();
@@ -30,4 +15,3 @@ router.post('/usuario/registrar', controlador.registrarUsuario);
 
 module.exports=router; // Exporta las rutas para que puedan ser utilizadas en index.js
 
->>>>>>> main
